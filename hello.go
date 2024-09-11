4959d80cbf17cba8636ef4df385a5106d73f8228
Philip Karanja <kpk.my.mailings@gmail.com>
2024-09-11 16:52:50 -0500

    finished saying hi

--- hello.go
+++ hello.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+//entry point for go, prints Hello, World
+// func main() {
+// 	fmt.Println("Hello, World!")
+// }
+
+/*
+	+ %s placeholder is used to represent a string value
+	+ Format string requires a string argument to substituted for the %s placeholder 
+		- the function below is missing that arguement
+*/
+// func main() {
+// 	fmt.Printf("Hello, %s!/n")
+// }
+
 func main() {
-fmt.Println("Hello, World!")
+	fmt.Printf("Hello, %s!/n,", "World")
 }
+
